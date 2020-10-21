44331443cdbc78b5841628b413fda4e7ed7ed7ac
zamzoum94 <mzemzemi9@gmail.com>
2020-10-21 17:07:46 +0100

    selecting data

--- functions/sanatize.php
+++ functions/sanatize.php
@@ -3,3 +3,15 @@
     function escape($str){
         return htmlentities($str, ENT_QUOTES, "utf-8");
     }
+
+    function splitArrayType($array, $props = array()){
+        $newArray = array();
+        foreach($props as $prop){
+            foreach($array as $element){
+                if($element->type === $prop){
+                    $newArray[$prop][] = $element;
+                }
+            }
+        }
+        return $newArray;
+    }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     include_once "core/init.php";
-    
+    $db = Db::getInstance();
+
     if(Input::exists("post")){
         var_dump($_POST);
         $validate = new Validation();
@@ -8,6 +9,12 @@
             
         }
     }
+
+    $data = $db->query("SELECT * FROM capteur 
+                        JOIN rcsf ON capteur.ref = rcsf.type 
+                        ORDER BY 'type'")->getResults();
+    
+    $data = splitArrayType($data, ["vanet", "uwsn"]);
 ?>
 
 <html>
@@ -15,7 +22,7 @@
         <title>Assessment</title>
     </head>
     <body>
-        <div>
+        <section>
             <h3>Ajout un capteur</h3>
             <form method="POST" action="">
                 <div>
@@ -36,6 +43,44 @@
                 </div>
                 <button type="submit">Ajouter</button>
             </form>
-        </div>
+        </section>
+        <hr>
+        <section>
+            <h3>Capteurs</h3>
+            <div>
+                <h4>Vanet(<?=count($data["vanet"]) ?>)</h4>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Latitude X</th>
+                        <th>Longitude Y</th>
+                    </tr>
+                    <?php foreach($data["vanet"] as $vanet){ ?>
+                    <tr>
+                        <td><?= $vanet->id ?></td>
+                        <td><?= $vanet->x_coordonne ?></td>
+                        <td><?= $vanet->y_coordonne ?></td>
+                    </tr>
+                    <?php } ?>
+                </table>
+            </div>
+            <div>
+                <h4>Uwsn(<?=count($data["uwsn"]) ?>)</h4>
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Latitude X</th>
+                        <th>Longitude Y</th>
+                    </tr>
+                    <?php foreach($data["uwsn"] as $uwsn){ ?>
+                    <tr>
+                        <td><?= $uwsn->id ?></td>
+                        <td><?= $uwsn->x_coordonne ?></td>
+                        <td><?= $uwsn->y_coordonne ?></td>
+                    </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </section>
     </body>
 </html>
